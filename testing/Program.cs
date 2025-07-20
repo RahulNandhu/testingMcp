92006a50b362f7b75e12603b5de3200d7db0dc20
RahulP <rahulpnandhu111@gmail.com>
2025-07-20 13:18:58 +0530

    changed swagger opton

--- testing/Program.cs
+++ testing/Program.cs
@@ -19,11 +19,10 @@ builder.Services.AddScoped<ICommonServices, CommonServices>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 //app.UseHttpsRedirection();
 
